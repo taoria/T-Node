a24331dc2e519b0e0e81c42d09b19b86c95e03f1
taoria <445625470@qq.com>
2022-06-27 17:05:47 +0800

    1.add an example to show

---
+++ Sample/Editor/HelloEditor.cs
@@ -0,0 +1,22 @@
+using TNode.Editor;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace Sample.Editor{
+    public class HelloEditor : GraphEditor<HelloGraph>{
+        [OnOpenAsset]
+        public static bool OnOpenAsset(int instanceID, int line){
+            var graph = EditorUtility.InstanceIDToObject(instanceID) as HelloGraph;
+            if (graph != null)
+            {
+                var wnd = GetWindow<HelloEditor>();
+                wnd.titleContent = new GUIContent("HelloGraph Editor");
+                wnd.CreateGUI();
+                wnd._graphView.Data = graph;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Sample/Editor/HelloGraph.cs
@@ -0,0 +1,11 @@
+using System;
+using TNode.Models;
+using UnityEngine;
+
+namespace Sample.Editor{
+    [CreateAssetMenu(fileName = "New HelloGraph", menuName = "TNode/HelloGraph")]
+    [Serializable]
+    public class HelloGraph : GraphData{
+    
+    }
+}
