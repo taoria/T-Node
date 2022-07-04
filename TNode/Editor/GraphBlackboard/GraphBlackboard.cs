160b9b03f607b61ff5648bb2ad3063b954cc7ff9
taoria <445625470@qq.com>
2022-07-05 00:18:04 +0800

    1.a bit foundation of Blackboard

--- TNode/Editor/GraphBlackboard/GraphBlackboard.cs
+++ TNode/Editor/GraphBlackboard/GraphBlackboard.cs
@@ -1,10 +1,12 @@
-﻿using UnityEditor.Experimental.GraphView;
+﻿using TNode.Models;
+using UnityEditor.Experimental.GraphView;
 
 namespace TNode.Editor.GraphBlackboard{
     /// <summary>
     /// Implement this class to create graph black board for specified graph
     /// </summary>
-    public class GraphBlackboard:Blackboard{
+    public class GraphBlackboard<T>:Blackboard where T:BlackboardData{
+        public T BlackboardData;
         
     }
 }
--- TNode/Models/BlackboardData.cs
+++ TNode/Models/BlackboardData.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
+﻿namespace TNode.Models{
 
-namespace TNode{
-    [Serializable]
     public class BlackboardData{
-        public SortedList<string, MonoBehaviour> Behaviours;
-        public SortedList<string, float> Floats;
-        public SortedList<string, int> Ints;
-        public SortedList<string, string> Strings;
+        
     }
 }
