1738d00b8c25a27771735fcf6aa35ee9d90f6897
taoria <445625470@qq.com>
2022-07-15 17:27:20 +0800

    feat: force blackboard select no scene object

---
+++ TNodeGraphViewImpl/Editor/GraphBlackboard/GraphBlackboardPropertyField.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace TNodeGraphViewImpl.Editor.GraphBlackboard{
+    public class GraphBlackboardPropertyField:PropertyField{
+        public GraphBlackboardPropertyField(SerializedProperty findPropertyRelative, string fieldName):base(findPropertyRelative, fieldName){
+            
+        }
+
+        protected override void ExecuteDefaultActionAtTarget(EventBase evt)
+        {
+            base.ExecuteDefaultActionAtTarget(evt);
+            if (this.Q<ObjectField>() != null){
+                this.Q<ObjectField>().allowSceneObjects = false;
+            }
+        }
+     
+        
+    }
+}
