985f5e5638dd39e37f316ea5f64642b72581e926
taoria <445625470@qq.com>
2022-08-18 17:50:46 +0800

    fix:add some test for TNode

--- TNodeCore/Runtime/Models/PortInfo.cs
+++ TNodeCore/Runtime/Models/PortInfo.cs
@@ -3,6 +3,9 @@
 namespace TNodeCore.Runtime.Models{
     [Serializable]
     public class PortInfo{
+        /// <summary>
+        /// Port entry name is port's name ,not the portName of the port
+        /// </summary>
         public string portEntryName;
         public string nodeDataId;
     }
